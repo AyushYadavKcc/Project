8b5601b48bde80f08eadb10bfe2113c68b6117c3
AyushYadavKcc <ayushy2112btechdscse2024@kccitm.edu.in>
2022-06-06 20:53:29 +0530

    routes

---
+++ routes/student.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  console.log(req)
+  res.json({name:"ayush"})
+});
+router.get('/:da-:ta', function(req, res, next) {
+      console.log(req.params)
+      res.json({name:req.params.da+","+req.params.ta+"="+req.params.da+req.params.ta})
+});
+module.exports = router;
